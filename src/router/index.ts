68887e20f419b6ea928a162cbf146a684c478d83
derlan <1372545550@qq.com>
2022-06-02 23:22:06 +0800

    refactor 使用import.meta.glob 无需再手动导入

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,64 +1,23 @@
-import { lazy } from 'react';
+import React, { lazy } from 'react';
 
-export const routes = [
-  {
-    path: '/pureComponent',
-    name: 'Pure组件',
-    component: lazy(() => import('../learn-component/pure')),
-  },
-  {
-    path: '/memoComponent',
-    name: 'memo组件',
-    component: lazy(() => import('../learn-component/memo')),
-  },
-  {
-    path: '/forwardRef',
-    name: 'forwardRef传递',
-    component: lazy(() => import('../learn-component/forwardRef')),
-  },
-  {
-    path: '/lazy',
-    name: '懒加载',
-    component: lazy(() => import('../learn-component/lazy')),
-  },
-  {
-    path: '/fragment',
-    name: 'fragment',
-    component: lazy(() => import('../learn-component/fragment')),
-  },
-  {
-    path: '/context',
-    name: 'context',
-    component: lazy(() => import('../learn-component/context')),
-  },
-  {
-    path: '/useState',
-    name: 'useState',
-    component: lazy(() => import('../learn-hooks/useState')),
-  },
-  {
-    path: '/useEffect',
-    name: 'useEffect',
-    component: lazy(() => import('../learn-hooks/useEffect')),
-  },
-  {
-    path: '/useMemo',
-    name: 'useMemo',
-    component: lazy(() => import('../learn-hooks/useMemo')),
-  },
-  {
-    path: '/useCallback',
-    name: 'useCallback',
-    component: lazy(() => import('../learn-hooks/useCallback')),
-  },
-  {
-    path: 'useLayoutEffect',
-    name: 'useLayoutEffect',
-    component: lazy(() => import('../learn-hooks/useLayoutEffect')),
-  },
-  {
-    path: 'useReducer',
-    name: 'useReducer',
-    component: lazy(() => import('../learn-hooks/useReducer')),
-  },
-];
+const learnComponent = import.meta.glob('../learn-component/*.tsx');
+const learnHooks = import.meta.glob('../learn-hooks/*.tsx');
+const learnDoms = import.meta.glob('../learn-dom/*.tsx');
+const learns = {
+  ...learnComponent,
+  ...learnHooks,
+  ...learnDoms,
+};
+export const routes:{
+  path: string,
+  component: React.LazyExoticComponent<React.FC>,
+  name: string,
+}[] = [];
+for (const path of Object.keys(learns)) {
+  const name = path.match(/\/(\w*).tsx/)![1]!;
+  routes.push({
+    name,
+    path: name,
+    component: lazy(learns[path] as any),
+  });
+}
